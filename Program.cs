a3250cef50e781795d47d6293699760cbd68ce74
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2020-05-30 01:42:57 -0300

    começando com dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_learning_path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# .NET Learning path:
+
+Criando um novo projeto
+
+        dotnet new --help
+
